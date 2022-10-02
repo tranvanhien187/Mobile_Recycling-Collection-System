4e22199b6004545746a226ffc493641f4d93cfc1
hien <tranvanhien187@gmail.com>
2022-10-02 20:41:47 +0700

    implement place detail ui

---
+++ app/src/main/java/datn/cnpm/rcsystem/common/widget/PrimaryButton.kt
@@ -0,0 +1,29 @@
+package datn.cnpm.rcsystem.common.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import datn.cnpm.rcsystem.R
+
+class PrimaryButton @JvmOverloads constructor(
+    context: Context,
+    attributes: AttributeSet,
+    defStyle: Int = 0,
+) : AppCompatButton(context, attributes, defStyle) {
+
+    companion object {
+        private const val DEFAULT_TEXT_SIZE = 16F
+    }
+
+    init {
+        background = ContextCompat.getDrawable(context, R.drawable.bg_primary_button)
+        setTextColor(ContextCompat.getColor(context, R.color.white))
+        typeface = ResourcesCompat.getFont(context, R.font.montserrat_semibold)
+        isAllCaps = false
+        gravity = Gravity.CENTER
+        textSize = DEFAULT_TEXT_SIZE
+    }
+}
